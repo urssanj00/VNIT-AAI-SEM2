2824825273af1b7f7dd55a8ec77583d0a7e5a790
Sanjeev <csaa.sanjeev@gmail.com>
2024-12-15 01:40:42 +0530

    data pull pm2_5
    Signed-off-by: Sanjeev <csaa.sanjeev@gmail.com>

--- SEM-2/ASP/LAB_EXPERIMENT_2/ConvolutionTest.py
+++ SEM-2/ASP/LAB_EXPERIMENT_2/ConvolutionTest.py
@@ -21,19 +21,37 @@ class ConvolutionTest:
         for i in range(x_len):
             for j in range(y_len):
                 result[i + j] += self.x[i] * self.y[j]
-                print(f"signal_manual_convolution: result[{i + j}] += {self.x[i]} * {self.x[j]} -> {result[i + j]}")
 
+        print(f"signal_manual_convolution:  -> {result }")
         return result
 
     def signal_api_convolution(self):
         numpy_result = np.convolve(self.x, self.y, 'full')
         return numpy_result
 
-    def signal_api_convolution(self):
+    def wave_signal_api_convolution(self):
         convolve_result = signal_convolve(self.x, self.y, 'full')
         return convolve_result
 
-    def plot_convolved_signal(self, manual_convolved_signal, api_convolved_signal, plot_file_name):
+    def plot_helper(self, signal_data, discrete, sub_plot, p_label, p_marker):
+        # Set bar width for thin bars
+        bar_width = 0.03
+
+        # Create a time range for plotting
+        time_signal = range(len(signal_data))
+
+        if discrete:
+            sub_plot.bar(time_signal, signal_data, width=bar_width, label=p_label)
+        else:
+            sub_plot.plot(time_signal, signal_data, label=p_label, marker=p_marker)
+
+        sub_plot.legend()
+        return sub_plot
+
+
+
+    def plot_convolved_signal(self, manual_convolved_signal, api_convolved_signal, plot_file_name,
+                              x_label, y_label, discrete=True):
 
         # Define the signals and convolution results
         signal_x = self.x
@@ -41,43 +59,35 @@ class ConvolutionTest:
         manual_convolution_1_of_x_and_y = manual_convolved_signal
         api_convolution_2_of_x_and_y = api_convolved_signal
 
-        # Create a time range for plotting
-        time_signal = range(len(signal_x))
-        time_convolution = range(len(manual_convolution_1_of_x_and_y))
-
         # Plotting
         figure = plt.figure(figsize=(13, 8))
         grid_spec = gridspec.GridSpec(3, 2, width_ratios=[1, 1])
 
         # Plot signal_x
         subplot_x = plt.subplot(grid_spec[0, 0])
-        subplot_x.plot(time_signal, signal_x, label='Signal X', marker='o')
-        subplot_x.legend()
+        subplot_x = self.plot_helper(signal_x, discrete, subplot_x, x_label, 'o')
 
         # Plot signal_y
         subplot_y = plt.subplot(grid_spec[0, 1])
-        subplot_y.plot(time_signal, signal_y, label='Signal Y', marker='x')
-        subplot_y.legend()
+        subplot_y = self.plot_helper(signal_y, discrete, subplot_y, y_label, 'x')
 
         # Plot manual convolution
         subplot_manual = plt.subplot(grid_spec[1, :])
-        subplot_manual.plot(time_convolution, manual_convolution_1_of_x_and_y, label='Manual Convolution', marker='s')
-        subplot_manual.legend()
+        subplot_manual = self.plot_helper(manual_convolution_1_of_x_and_y, discrete,
+                                          subplot_manual,'Manual Convolution', 's')
 
         # Plot manual convolution
         subplot_api = plt.subplot(grid_spec[2, :])
-        subplot_api.plot(time_convolution, api_convolution_2_of_x_and_y, label='API Convolution', marker='d')
-        subplot_api.legend()
+        subplot_api = self.plot_helper(api_convolution_2_of_x_and_y, discrete,
+                                       subplot_api, 'Built-in Convolution', 'd')
 
         plt.tight_layout()
         plt.savefig(plot_file_name)
         plt.close()
-        # Display the plot
-        #plt.show()
 
 
 # (a) x1[n] = [1, 1, 2, 3] and x2[n] = [0.2, 0.5, 0.3]
-x = [1, 2, 3]
+x = [1, 1, 2, 3]
 y = [0.2, 0.5, 0.3]
 
 convolutionTest_a = ConvolutionTest(x, y)
@@ -85,9 +95,11 @@ manual_result = convolutionTest_a.signal_manual_convolution()
 api_result = convolutionTest_a.signal_api_convolution()
 
 print(f'Manual Convolution : {manual_result}')
-print(f'API Convolution : {api_result.tolist()}')
+print(f'Built-in Convolution : {api_result.tolist()}')
 
-convolutionTest_a.plot_convolved_signal(manual_result, api_result, 'plot_q_a.png')
+convolutionTest_a.plot_convolved_signal(manual_result, api_result, 'plot_q_a.png',
+                                        'x1[n] = [1, 1, 2, 3]',
+                                        'x2[n] = [0.2, 0.5, 0.3]', True)
 
 # (b) x1(t) = sin(t) and x2(t) = cos(t)
 
@@ -98,9 +110,11 @@ y = np.cos(t)
 
 convolutionTest_b = ConvolutionTest(x, y)
 manual_result = convolutionTest_b.signal_manual_convolution()
-api_result = convolutionTest_b.signal_api_convolution()
+api_result = convolutionTest_b.wave_signal_api_convolution()
 
 print(f'Manual Convolution : {manual_result}')
-print(f'API Convolution : {api_result.tolist()}')
+print(f'Built-in Convolution : {api_result.tolist()}')
 
-convolutionTest_b.plot_convolved_signal(manual_result, api_result, 'plot_q_b.png')
+convolutionTest_b.plot_convolved_signal(manual_result, api_result, 'plot_q_b.png',
+                                        'x1(t) = sin(t)',
+                                        'x2(t) = cos(t)', False)
---
+++ SEM-2/MINI_PROJECT/WORKSPACE/com/data-prep/URLToCSV.py
@@ -0,0 +1,84 @@
+import requests
+import csv
+from datetime import datetime
+import csv
+import ijson
+from PropertiesConfig import PropertiesConfig as PC
+import pandas as pd
+
+class URLToCSV:
+    def __init__(self):
+        # URL of the JSON data
+        # Create an instance of PropertiesConfig
+        properties_config = PC("sensor-data.properties")
+        # Get properties as a dictionary
+        self.properties = properties_config.get_properties_config()
+
+        self.station_list_url = self.properties['station_list_url']
+        self.data_set_dir = self.properties['data_set_path']
+        # Generate a timestamp for the filename
+        timestamp = datetime.now().strftime(self.properties['time_format'])
+        current_date = datetime.now().strftime(self.properties['date_format'])
+        self.station_list_csv_file = f"{self.data_set_dir}/station_list_{current_date}.csv"
+        self.station_list_df = None
+        self.station_url_list = []
+
+    def dump_csv(self, url, file_path):
+        #file_path = self.station_list_csv_file
+
+        # Stream JSON from URL
+        response = requests.get(url, stream=True)
+
+        if response.status_code == 200:
+            # Open the CSV file for writing
+            with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+                writer = None
+
+                # Stream parse the JSON data
+                for record in ijson.items(response.raw, 'item'):
+                    # Initialize CSV writer and write header once
+                    if writer is None:
+                        writer = csv.DictWriter(file, fieldnames=record.keys())
+                        writer.writeheader()
+
+                    # Write each record
+                    writer.writerow(record)
+
+            print(f"Data has been written to {file_path}")
+        else:
+            print(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+
+    def m1_dump_station_data_list(self):
+        self.dump_csv(self.station_list_url, self.station_list_csv_file)
+
+    def m2_read_station_list_in_df(self):
+        # Read the CSV file
+        self.station_list_df = pd.read_csv(self.station_list_csv_file)
+
+        # Display the first few rows of the dataframe
+        print(self.station_list_df.head())
+
+    def m3_build_station_url_list(self):
+        st_id_df = self.station_list_df['_id']
+
+        for st_id in st_id_df:
+            print(st_id)
+            self.station_url_list.append(f"{self.station_list_url}/{st_id}/sensorData?days=")
+
+    def m4_dump_station_pm2_5_data(self, days):
+        i=0
+        for st_id in self.station_url_list:
+            url_st_data = st_id + str(days)
+            filename = f'{self.data_set_dir}/{i}.csv'
+            print(filename)
+            self.dump_csv(url_st_data, filename)
+            i = i+1
+
+urlToCSV = URLToCSV()
+urlToCSV.m1_dump_station_data_list()
+urlToCSV.m2_read_station_list_in_df()
+
+urlToCSV.m3_build_station_url_list()
+urlToCSV.m4_dump_station_pm2_5_data(1)
+
+print(f"{urlToCSV.station_url_list}")
